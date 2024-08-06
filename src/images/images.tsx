338b378b5e025efd72c2d42d375bc567afdee2e0
Haritiana Adriano <103512767+haritianaadriano@users.noreply.github.com>
2024-08-06 16:18:23 +0300

    feat: use backend login service

--- src/images/images.tsx
+++ src/images/images.tsx
@@ -1,6 +1,6 @@
 const IMAGES = {
   loginImage: new URL("./3293465.jpg", import.meta.url).href,
-  enterpriseLogo: new URL("./DEBUGOO.png", import.meta.url).href
+  enterpriseLogo: new URL("./DEBUGOO.png", import.meta.url).href,
 };
 
 export default IMAGES;
